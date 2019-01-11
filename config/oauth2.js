cc8d124d815c82356533aecb386236c041f2f979
Nidhish Puthiyadath <nidhish.nambiar@gmail.com>
2019-01-10 16:30:29 -0800

    Update oauth2.js
    reverrt

--- config/oauth2.js
+++ config/oauth2.js
@@ -1,6 +1,6 @@
 module.exports = {
     clientID: 190308505162921,
     clientSecret: '351fb1ca2d58071dd748077102c3211a',
-    callbackURL: 'https://employ-app.herokuapp.com/login/facebook/return',
+    callbackURL: 'http://localhost:8000/login/facebook/return',
     profileFields: ['id', 'displayName', 'photos', 'email']
 }
